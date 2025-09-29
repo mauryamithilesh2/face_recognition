dddf625a95975eba88e4fc5f5c94a6b06b99e22b
Mithilesh Maurya <mauryamithilesh1@gmail.com>
2025-09-29 14:31:01 +0530

    Update settings.py

--- face_attendance/settings.py
+++ face_attendance/settings.py
@@ -149,7 +149,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://*.up.railway.app',
     'https://*.railway.app',
-    'https://facemark.up.railway.app',
+    'https://web-production-e5f76.up.railway.app',
 
 ]
 
